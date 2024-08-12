d533364b9fd6fd3bd9036031dc6a40c533cf5827
Junior <57546609+MJunior10@users.noreply.github.com>
2024-08-12 13:54:16 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:postgres@localhost:5433/usuarios'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5433/usuarios'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
